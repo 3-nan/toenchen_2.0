82616f6188f37880d56534d073ba18a3e4a47289
franzmotzkus <franzmotzkus@campus.tu-berlin.de>
2020-01-08 16:24:27 +0100

    start making content more dynamic

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        description: 'Wir sind elf Freunde, die viel Spaß am gemeinsamen Singen haben. Kennengelernt im Studierendenchor (denn jede*r von uns ist Studierende*r oder war es zumindest mal), hatten wir Lust auf "mehr": Mehr Stimmbildung, mehr Performance und mehr Zeit zum Vertiefen unserer Musikalität. Aus den ersten gemeinsamen Gesangsabenden wurden regelmäßige Proben, gefolgt von kleinen Auftritten, die größer und länger wurden. Mittlerweile besingen wir auch Weihnachtsfeiern oder Hochzeiten und haben im April 2018 ein eigenes Konzert in Paris aufführen dürfen. Im Repertoire haben wir eine gute Mischung aus alten und neuen Klassikern der Popmusik, einige romantische Balladen und auch Disney-Songs.',
+        members: 'Claudia, Franz, Gönke, Han, Jette, Marc, Mette, Milena, Phillipp, Thilo und Timo',
+    });
 });
 
 router.get('/admin', (req, res) => {
